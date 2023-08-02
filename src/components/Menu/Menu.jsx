ff2d9c50bfa4f207d462d4b56bfec7df5ab21576
luckyowl11 <sudhanshuseenver3.0@gmail.com>
2023-08-02 19:49:37 +0530

    menu initiatives

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import styles from "./Menu.module.css";
 import Button from "../Button/Button";
-import { MdOutlineArrowForwardIos, MdOutlineClose } from "react-icons/md";
+import {
+  MdOutlineArrowForwardIos,
+  MdOutlineClose,
+  MdOutlineArrowBackIos,
+} from "react-icons/md";
 import { ReactComponent as BD_Logo } from "../../assets/images/logo.svg";
 import { initiatives_data } from "../../helpers/data";
 import InitiativeCards from "../InitiativeCards/InitiativeCards";
@@ -9,6 +13,12 @@ import InitiativeCards from "../InitiativeCards/InitiativeCards";
 function Menu({ showMenu, closeMenu, innerWidth, ...props }) {
   const menuToggleClass = showMenu ? styles.open_menu_ct : styles.close_menu_ct;
   const showCarousel = innerWidth > 1280 ? true : false;
+
+  const [toggleInitiatives, setToggleInitiatives] = useState(false);
+
+  function handleClickInitiatives(e) {
+    setToggleInitiatives(!toggleInitiatives);
+  }
 
   const menu_listData = [
     "WORK",
@@ -26,31 +36,19 @@ function Menu({ showMenu, closeMenu, innerWidth, ...props }) {
   return (
     <div className={`${styles.menu_ct} ${menuToggleClass}`}>
       <div className={styles.menu_head}>
-        <figure className={styles.menu_logo}>
-          <BD_Logo />
-        </figure>
+        {showCarousel ? (
+          <div></div>
+        ) : (
+          <figure className={styles.menu_logo}>
+            <BD_Logo />
+          </figure>
+        )}
 
         <Button onClick={handleClose} custCls={styles.btn_men}>
           <MdOutlineClose />
         </Button>
       </div>
-      {showCarousel ? (
-        <div className={styles.initiatives_Ct}>
-          <ol className={styles.initiatives_list}>
-            {initiatives_data.map((data, i) => {
-              return (
-                <li key={data.name}>
-                  <InitiativeCards
-                    data={data}
-                    lastOfType={i + 1 === initiatives_data.length}
-                    ithVal={i + 1}
-                  />
-                </li>
-              );
-            })}
-          </ol>
-        </div>
-      ) : (
+      {!showCarousel && !toggleInitiatives && (
         <div className={styles.menu_nav}>
           <ul className={styles.menu_navList}>
             {menu_listData.map((data) => (
@@ -59,7 +57,10 @@ function Menu({ showMenu, closeMenu, innerWidth, ...props }) {
               </li>
             ))}
             <li>
-              <Button custCls={styles.menuInitiatives_btn}>
+              <Button
+                onClick={handleClickInitiatives}
+                custCls={styles.menuInitiatives_btn}
+              >
                 INITIATIVES
                 <span>
                   <MdOutlineArrowForwardIos />
@@ -69,6 +70,53 @@ function Menu({ showMenu, closeMenu, innerWidth, ...props }) {
           </ul>
         </div>
       )}
+      {(showCarousel || toggleInitiatives) && (
+        <div className={styles.initiatives}>
+          <div className={styles.info_initiatives}>
+            {toggleInitiatives && !showCarousel && (
+              <div
+                onClick={handleClickInitiatives}
+                className={styles.info_back}
+              >
+                <Button custCls={styles.info_backBtn}>
+                  <MdOutlineArrowBackIos />
+                </Button>
+              </div>
+            )}
+            <div className={styles.info}>
+              <span>&#11044;</span>
+              <p>
+                (5) internal works
+                <br />
+                &copy; <span>c/o</span> Basic/dept&reg;
+              </p>
+              <p>
+                A collection of internal project and initiatives under the
+                BASIC/DEPT® brand.
+              </p>
+            </div>
+          </div>
+          <div className={styles.initiatives_Ct}>
+            <ol className={styles.initiatives_list}>
+              {initiatives_data.map((data, i) => {
+                return (
+                  <li key={data.name}>
+                    <InitiativeCards
+                      data={data}
+                      lastOfType={i + 1 === initiatives_data.length}
+                      ithVal={i + 1}
+                    />
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
+        </div>
+      )}
+      <div className={styles.menu_copyright}>
+        <span>BASIC/DEPT&reg;, INC</span>
+        <span>10 - {String(new Date().getFullYear()).slice(-2)}&copy;</span>
+      </div>
     </div>
   );
 }
